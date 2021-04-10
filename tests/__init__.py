4e6835fe43e315f9bc879fc1e6a74576dcdefb3c
Denis Talakevich <senid231@gmail.com>
2021-04-10 13:30:56 +0300

    add black code formatting

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,9 +8,15 @@ from Cryptodome.Signature import pss
 
 def decrypt(data: bytes, private_key: str, key_index: int):
     key = RSA.import_key(private_key)
-    cipher_rsa = PKCS1_OAEP.new(key=key, hashAlgo=SHA256, mgfunc=lambda x, y: pss.MGF1(x, y, SHA256))
-    encrypted_aes_key_iv = data[0:512] if key_index == 0 else data[512:1024]  # RSA 4096 OAEP encrypts to 512 bytes
-    aes_key_iv = cipher_rsa.decrypt(encrypted_aes_key_iv)  # 32 bytes aes_key + 16 bytes aes_iv
+    cipher_rsa = PKCS1_OAEP.new(
+        key=key, hashAlgo=SHA256, mgfunc=lambda x, y: pss.MGF1(x, y, SHA256)
+    )
+    encrypted_aes_key_iv = (
+        data[0:512] if key_index == 0 else data[512:1024]
+    )  # RSA 4096 OAEP encrypts to 512 bytes
+    aes_key_iv = cipher_rsa.decrypt(
+        encrypted_aes_key_iv
+    )  # 32 bytes aes_key + 16 bytes aes_iv
     cipher_aes = AES.new(aes_key_iv[0:32], AES.MODE_CBC, aes_key_iv[32:])
     return unpad(cipher_aes.decrypt(data[1024:]), AES.block_size)
 
--- tests/test_Encrypt.py
+++ tests/test_Encrypt.py
@@ -3,21 +3,25 @@ import unittest
 import httpretty
 from tests import decrypt, read_fixture
 from Cryptodome.Random import get_random_bytes
-from didww_encrypt import Encrypt, URI_SANDBOX, URI_PRODUCTION, MODE_SANDBOX, MODE_PRODUCTION
+from didww_encrypt import (
+    Encrypt,
+    URI_SANDBOX,
+    URI_PRODUCTION,
+    MODE_SANDBOX,
+    MODE_PRODUCTION,
+)
 
 
 class TestEncrypt(unittest.TestCase):
     @httpretty.activate(allow_net_connect=False)
     def test_initialize_mode_sandbox(self):
         httpretty.register_uri(
-            httpretty.GET,
-            URI_SANDBOX,
-            body=read_fixture("public_keys.json")
+            httpretty.GET, URI_SANDBOX, body=read_fixture("public_keys.json")
         )
         enc = Encrypt.new(mode=MODE_SANDBOX)
         self.assertEqual(
             "ca5af2d14bee923a0a0d1687b7c77e7211a57f84:::683150ee69b4d906aa883d0ac12b0fdd79f95bcf",
-            enc.fingerprint
+            enc.fingerprint,
         )
         data = get_random_bytes(1024 * 1024)  # 1MB
         encrypted = enc.encrypt(data)
@@ -30,14 +34,12 @@ class TestEncrypt(unittest.TestCase):
     @httpretty.activate(allow_net_connect=False)
     def test_initialize_mode_production(self):
         httpretty.register_uri(
-            httpretty.GET,
-            URI_PRODUCTION,
-            body=read_fixture("public_keys.json")
+            httpretty.GET, URI_PRODUCTION, body=read_fixture("public_keys.json")
         )
         enc = Encrypt.new(mode=MODE_PRODUCTION)
         self.assertEqual(
             "ca5af2d14bee923a0a0d1687b7c77e7211a57f84:::683150ee69b4d906aa883d0ac12b0fdd79f95bcf",
-            enc.fingerprint
+            enc.fingerprint,
         )
         data = get_random_bytes(1024 * 1024)  # 1MB
         encrypted = enc.encrypt(data)
@@ -51,18 +53,16 @@ class TestEncrypt(unittest.TestCase):
     def test_initialize_custom_uri(self):
         uri = "https://api.example.com/keys"
         httpretty.register_uri(
-            httpretty.GET,
-            uri,
-            body=read_fixture("public_keys.json")
+            httpretty.GET, uri, body=read_fixture("public_keys.json")
         )
         enc = Encrypt.new(uri=uri)
         self.assertEqual(
             "ca5af2d14bee923a0a0d1687b7c77e7211a57f84:::683150ee69b4d906aa883d0ac12b0fdd79f95bcf",
-            enc.fingerprint
+            enc.fingerprint,
         )
         data = get_random_bytes(1024 * 1024)  # 1MB
         encrypted = enc.encrypt(data)
-        private_keys = json.loads(read_fixture('private_keys.json'))
+        private_keys = json.loads(read_fixture("private_keys.json"))
         decrypted_a = decrypt(encrypted, private_keys["private_key_a"], 0)
         self.assertEqual(data, decrypted_a)
         decrypted_b = decrypt(encrypted, private_keys["private_key_b"], 1)
